0b3a86c91ec2ad9d2168c63b5bcb16b8b048560d
sanchezle <65270219+sanchezle@users.noreply.github.com>
2022-09-27 23:53:13 +0200

    textarea creation

--- contro.js
+++ contro.js
@@ -1,2 +1,11 @@
 const boton = getElementById('boton');
 const parrafo = getElementById('post');
+
+
+//function where the post is created
+boton.onmouseclicK("mouseclick", textArea);
+//it has to creat a h2 element with the input of the user
+function textArea(){
+    document.createElement("textarea")
+}
+function post_uno(){}
