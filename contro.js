eab1878cf4485f153e8aec9aa52de0bc6b992f7e
sanchezle <65270219+sanchezle@users.noreply.github.com>
2022-10-27 22:07:48 +0200

    new margin

--- contro.js
+++ contro.js
@@ -9,8 +9,7 @@ function textArea(){
     document.createElement("textarea")
 }
 function create_p(textArea){
-    doment.createElement("h2");
-
+    document.createElement("h2");
 }
 
 //the text area is already created you have to create a function tha takes texarea input and log it as h2 to the site.
