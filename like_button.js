e3bcbcc46393c52e24e8d2673909988eaf61b990
sanchezle <sanchezle21@gmail.com>
2023-08-22 18:11:25 +0200

    like button second try

--- like_button.js
+++ like_button.js
@@ -1,3 +1,16 @@
+
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
+
+function LikeButton() {
+    // ... component code here
+    return (
+        <button>Like</button>
+    );
+}
+
+const e = React.createElement;
+
 root.render(e(LikeButton));
+
+
