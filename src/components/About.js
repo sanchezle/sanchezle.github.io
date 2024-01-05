11bf8a7cc1e4ffe9f83987fa2f12c275d06fb801
sanchezle <sanchezle21@gmail.com>
2024-01-05 13:49:25 +0100

    pic and description added

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Luis from './images/luis-sanchez.png'
+import './About.css'
+
+const About = () => {
+    return (
+        <div className='about' id='about'>
+            <div className='container'>
+                <img src={Luis} alt='Luis Sñanchez' style={{width: "600px", height: "auto"}} />
+                <div className='col-2'>
+                    <h2>About</h2>
+                    <span className='line'></span>
+                    <p>Software Developer with Harvard's CS50 foundation in computing. Proficient in JavaScript, HTML, CSS, React, MongoDB, Node.js, Express.js, Python, Flask, and C. Experienced in MERN stack applications and software projects from inception to completion. Passionate about applying skills in the tech industry.
+
+
+
+
+
+.</p>
+                    <p> Hi iam Luis welcome to my page, I hope you like it.</p>
+                    <button className='button'>Explore More</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
