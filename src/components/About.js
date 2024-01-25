c1e2a6953b6ef13e04745bbc08d7dfb1a089c5a9
sanchezle <sanchezle21@gmail.com>
2024-01-25 15:52:48 +0100

    _config.yml file fore github jekyll deployment

--- src/components/About.js
+++ src/components/About.js
@@ -13,9 +13,6 @@ const About = () => {
                     <p>Software Developer with Harvard's CS50 foundation in computing. Proficient in JavaScript, HTML, CSS, React, MongoDB, Node.js, Express.js, Python, Flask, and C. Experienced in MERN stack applications and software projects from inception to completion. Passionate about applying skills in the tech industry.
 
 
-
-
-
 .</p>
                     <p> Hi iam Luis welcome            to my page, I hope you like it.</p>
                     <button className='button'>Explore More</button>
